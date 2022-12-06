de3d1489e313509cb5eddfa751d9238b013fc520
Andrew Huffman <ahuffman@cedarville.edu>
2022-12-05 22:49:34 -0500

    Fixed?

--- src/tomasulogui/IntMult.java
+++ src/tomasulogui/IntMult.java
@@ -9,22 +9,6 @@ public class IntMult extends FunctionalUnit {
     }
 
     public int calculateResult(int station) {
-        // assumption that the reservation stations have all data needed
-        
-        // get contents at specific station
-        // if operation is *
-            // mult two operands in reservation station
-        // else
-            // through an exception
-        // update CDB (possible reorder buffer/regs individually?) with result 
-        // of valid data from the reorder buffer and tag
-        
-        // (may have race condition on who should write to the CDB if mult and 
-        // int units finish at the same time)    
-        
-        // return result of calculation 
-        
-        // place holder code
         int result = 0;
         return result;
 
