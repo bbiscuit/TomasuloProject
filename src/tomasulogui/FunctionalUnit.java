bde35f9cd1df541c8e1de031d290de4fcf0947d3
paynejames <81314722+paynejames@users.noreply.github.com>
2022-12-05 22:06:41 -0500

    Comments for IntAlu (#5)
    * Progress on FunctionalUnit
    
    * A few more changes to Functional Unit
    
    * Started on int ALU calc result function
    
    * I think the IntAlu is about ready
    
    * Finished final touches on IntAlu comments

--- src/tomasulogui/FunctionalUnit.java
+++ src/tomasulogui/FunctionalUnit.java
@@ -1,31 +1,72 @@
 package tomasulogui;
 
 public abstract class FunctionalUnit {
-  PipelineSimulator simulator;
-  ReservationStation[] stations = new ReservationStation[2];
-  
-  public FunctionalUnit(PipelineSimulator sim) {
-    simulator = sim;
-    
-  }
 
- 
-  public void squashAll() {
-    // todo fill in
-  }
+    PipelineSimulator simulator;
+    ReservationStation[] stations = new ReservationStation[2];
 
-  public abstract int calculateResult(int station);
+    public FunctionalUnit(PipelineSimulator sim) {
+        simulator = sim;
+    }
 
-  public abstract int getExecCycles();
+    public void squashAll() {
+        // todo fill in
+        
+        // empty all reservation stations of contents b/c of miss predict
+            // empty every operand
+            // and all operation field of all reservation stations
+    }
 
-  public void execCycle(CDB cdb) {
-    //todo - start executing, ask for CDB, etc.
-  }
+    public abstract int calculateResult(int station);
 
+    public abstract int getExecCycles();
 
+    public void execCycle(CDB cdb) {
+        //todo - start executing, ask for CDB, etc.
+        
+        // not sure what this does in here
+        
+    }
 
-  public void acceptIssue(IssuedInst inst) {
-  // todo - fill in reservation station (if available) with data from inst
-  }
+    public void acceptIssue(IssuedInst inst) {
+        // todo - fill in reservation station (if available) with data from inst
 
+        // working with/inputs:
+            // CDB
+            // virtual regs (FP Registers) sources tags or values (operands)
+            // operation from inst beign issued
+            
+        // if execution in ALUs int or mult are complete
+            // clear reservation stations of last executed inst and mark as open
+                
+        // if the reservation station is avaliable load data from the FP registers    
+            // load into open adder reservation stations if int
+                // update:
+                // update/mark current reservation station in use
+                // check the source operand fields 
+                // if virtual register at tag = busy, wait until oper are avaliable 
+                    // assign the reservation station with tag of virutal reg 
+                    // that it is waiting on
+                // else 
+                    // get operand from virtual registers at tag from inst
+                    //      OR from (check) CDB if virtual reg at tag is what we need
+                    //      instead of getting it from the virtual reg
+            // load into open mult reservation stations if mult
+                // working with (same as above just for mult reservation stations):
+                // update (same as above just for mult reservation stations):
+        // else no space
+            // stall inst b/c no room in specific reservation station
+        
+        // NOTES:
+            //      if both the int and mult reservation stations are needing
+            //          the same value, does it come from CDB or is it first come
+            //          first serve with regards to the tag?? (up to the designers?)
+            //      Example:
+            //          SUB F5 F1 F2    
+            //          MUL F3 F2 F4
+        // DECIDE: does this functional unit update which virtual regs are busy?
+        //         who updates who?
+        
+        // good video resource https://www.youtube.com/watch?v=jyjE6NHtkiA
+    }
 }
--- src/tomasulogui/IntAlu.java
+++ src/tomasulogui/IntAlu.java
@@ -1,20 +1,36 @@
 package tomasulogui;
 
-public class IntAlu extends FunctionalUnit{
-  public static final int EXEC_CYCLES = 1;
+public class IntAlu extends FunctionalUnit {
 
-  public IntAlu(PipelineSimulator sim) {
-    super(sim);
-  }
+    public static final int EXEC_CYCLES = 1;
 
+    public IntAlu(PipelineSimulator sim) {
+        super(sim);
+    }
 
-  public int calculateResult(int station) {
-     // just placeholder code
-    int result=0;
-    return result;
-  }
+    public int calculateResult(int station) {
+        // assumption that the reservation stations have all data needed
+        
+        // get contents at specific station
+        // if operation is +
+            // add two operands in reservation station
+        // else if operatoin is -
+            // subtract two operands in reservation station
+        // else
+            // throw an exception
+        // update CDB with result of valid data from the reorder buffer and tag
+        // may have race condition like IntMult and may need to update reorder buf
+        // and regs file (not sure if this is the same thing as updating the CDB)
+        
+        // return result of calculation
+        
+        // just placeholder code
+        int result = 0;
+        return result;
+    }
 
-  public int getExecCycles() {
-    return EXEC_CYCLES;
-  }
+    public int getExecCycles() {
+        // not sure if this is the only thing for this function
+        return EXEC_CYCLES;
+    }
 }
